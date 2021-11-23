7086edab6097a135d856d1230b847eea65c4fad6
Elizabeth A. Barnes <eabarnes@colostate.edu>
2021-11-23 14:07:15 -0700

    updated with mjo use case

--- experiment_settings.py
+++ experiment_settings.py
@@ -1,4 +1,4 @@
-"""This Looks Like That experimental settings"""
+"""This Looks Like That There experimental settings"""
 
 import numpy as np
 
@@ -56,6 +56,52 @@ def get_settings(exp_name):
                
                         'analyze_stage'      : 5,               
                        },    
+           'mjo': {
+                        'data_name'           : 'mjo',
+                        'data_dir'            : './data/',         
+                        'colored'             : False, 
+                        'standardize'         : 'all',
+                        'shuffle'             : False,
+                
+                        'nclasses'            : 9,
+                        'nlayers'             : 3, 
+                        'nfilters'            : [16,16,16],
+                        'double_conv'         : False,
+                
+                        'prototypes_per_class': [10,10,10,10,10,10,10,10,10],
+                
+                        'pretrain'            : True,
+                        'pretrain_exp'        : None,
+                        'nepochs_pretrain'    : 100, 
+                        'lr_cb_epoch_pretrain': 100,                
+                        'lr_pretrain'         : 0.00017548,            
+                        'pretrain_patience'   : 5,                
+                        'train_cnn_in_stage'  : True,
+                               
+                        'nepochs'             : np.ones(15,dtype=np.int8)*10,
+                        'lr_cb_epoch'         : 1000,                
+                        'lr'                  : 0.01,
+                        'cut_lr_stage'        : 4,   
+                        'min_lr'              : 1.e-20,               
+                        'batch_size'          : 32,
+                        'random_seed'         : 30,
+                        'batch_size_predict'  : 1_024, 
+                        'patience'            : 5,
+                
+                        'incorrect_strength'  : -0.5,                
+                        'coeff_cluster'       :  0.2,
+                        'coeff_separation'    : -0.2/10.,
+                        'coeff_l1'            : 0.1,
+                        'dense_nodes'         : 32,
+                        'prototype_channels'  : 64,
+                        'kernel_l1_coeff'     : 0.00,
+                        'kernel_l2_coeff'     : 0.00,
+                        'drop_rate'           : 0.4,
+                        'drop_rate_final'     : 0.2,
+               
+                        'analyze_stage'      : 9,               
+                       },        
+        
 
     }
     
